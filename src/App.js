679ae03c8302214a30170fb68622fe6073ff82f6
DonAzi <savaSh-00@mail.ru>
2022-01-18 08:42:59 +0000

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Profile from "./Profile";
+import Sidebar from "./components/profile/Sidebar";
+import { Col } from "react-bootstrap";
 
 function App() {
   return (
@@ -10,6 +12,9 @@ function App() {
       <a href="/profile">
         <h1>hefisgdisbdk</h1>
       </a>
+      <Col>
+        <Sidebar />
+      </Col>
       <Routes>
         <Route path="/profile" element={<Profile />} />
       </Routes>
---
+++ src/components/profile/PeopleViewed.jsx
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import { Col, Row } from "react-bootstrap";
+import { IoIosArrowDown } from "react-icons/io";
+import "./SideBar.css";
+
+const PeopleViewed = () => {
+  const [showMore, setShowMore] = useState(5);
+  const [info, setInfo] = useState([]);
+
+  let fetchUser = async () => {
+    try {
+      let apiCall = await fetch(
+        "https://striveschool-api.herokuapp.com/api/profile/",
+        {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MWU1M2IwMjczZDVjYjAwMTUzOTVhYTEiLCJpYXQiOjE2NDI0MTI4MDIsImV4cCI6MTY0MzYyMjQwMn0.Rbm3B63oIh5acqeuTn3D6U538MLbS0-vfoNT62fpFGA",
+          },
+        }
+      );
+      console.log(apiCall);
+
+      if (apiCall.ok) {
+        let user = await apiCall.json();
+        console.log(user);
+        setInfo(user);
+      } else {
+        console.log("Something wrong");
+      }
+    } catch (err) {
+      console.log("error Catched");
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <div>
+      <Row>
+        <Col className="pl-0 pr-0">
+          <section className="peopleView mt-4">
+            <ul className="pl-0 mb-0 w-100">
+              <div>
+                {" "}
+                <h2 className="text-heading-medium pt-3">People also viewed</h2>
+              </div>
+              {info.slice(0, showMore).map((u) => (
+                <li className="" key={u._id}>
+                  {" "}
+                  <a className="text-dark font-weight-bolder d-flex" href="/">
+                    <img className="img-fluid" src={u.image} alt="user" />
+                    <div className="mt-3">
+                      {u.name} <n />
+                      {u.surname}
+                      <p className="text-secondary">{u.title}</p>
+                    </div>
+                  </a>
+                  <div className="">
+                    <button className="btn btn-connect ">Connect</button>
+                  </div>
+                </li>
+              ))}
+              <div className="button-show">
+                <button id="btn" onClick={(e) => setShowMore(showMore + 5)}>
+                  Show more
+                </button>
+                <IoIosArrowDown className="text-secondary" />
+              </div>
+            </ul>
+          </section>
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default PeopleViewed;
