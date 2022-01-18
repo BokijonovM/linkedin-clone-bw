a3f6641a36bd1682c4aff4ce565ab8fb2c157e86
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-01-18 09:40:32 +0100

    updated

--- src/components/profile/MyMain.jsx
+++ src/components/profile/MyMain.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import Main from "./Main";
 import Sidebar from "./Sidebar";
 import { Row, Col } from "react-bootstrap";
-import './Style/profile.css'
+import "./style/profile.css";
 function MyMain() {
   return (
     <div>
-      <Row className="justify-content-center" style={{backgroundColor:'rgb(243,242,238)'}}>
+      <Row
+        className="justify-content-center"
+        style={{ backgroundColor: "rgb(243,242,238)" }}
+      >
         <Col sm={10} lg={7}>
           <Main />
         </Col>
