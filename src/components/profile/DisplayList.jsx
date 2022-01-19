ebc1f2e73eaa831ee15977f87b6e2118d3c273ef
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-01-19 08:37:23 +0000

    experiences displayed

--- src/components/profile/DisplayList.jsx
+++ src/components/profile/DisplayList.jsx
@@ -1,29 +1,48 @@
-import React, {Component} from 'react'
-import {GoThreeBars} from 'react-icons/go'
-import {GrFormEdit} from 'react-icons/gr'
+import React from "react";
+import { GoThreeBars } from "react-icons/go";
+import { GrFormEdit } from "react-icons/gr";
+import { format, parseISO } from "date-fns";
 
 class DisplayList extends React.Component {
-    render() { 
-        return <div className='d-flex justify-content-between  mt-2'>
-                <div style={{width:'120px',height:'120px', overflow:'hidden'}}>
-                    {/* <img src='https://image.shutterstock.com/image-vector/color-square-composition-text-geometric-600w-1337231156.jpg' className="w-100"/> */}
-                    <img src={this.props.list.image} alt={this.props.list.info} className='w-100'/>
-                </div>
-                <div className='w-100 ml-3 grey-border-bottom'>
-                    <p className='h6 bold p-0 m-0'>{this.props.list.title}</p>
-                    <p className='p-0 m-0 font14'>{this.props.list.info}</p>
-                    <p className='p-0 m-0 font14'> {this.props.list.year}</p>
-                    <span>{this.props.list.address}</span>
-                    
-                </div>
-                <div>
-                <span className="h3 text-primary dRelative"><GrFormEdit/></span>
+  render() {
+    return (
+      <div className="d-flex justify-content-between  ">
+        <div style={{ width: "80px", height: "80px", overflow: "hidden" }}>
+          <img
+            src="https://image.shutterstock.com/image-vector/color-square-composition-text-geometric-600w-1337231156.jpg"
+            className="w-100 mt-2"
+          />
+          {/* <img src={this.props.list.image} alt="" className="w-100" /> */}
+        </div>
+        <div className="w-100 ml-3">
+          <p className="h5 bold p-0 m-0">{this.props.list.role}</p>
+          <p className="p-0 m-0">{this.props.list.company}</p>
+          {/* <p className="p-0 m-0 font14">{this.props.list.description}</p> */}
+          <p className="p-0 m-0 font14">
+            {this.props.list.startDate}-{this.props.list.endDate}
+          </p>
+          <span>{this.props.list.area}</span>
+          <hr />
+          {/* "": "CTO",
+    "company": "Strive School",
+    "startDate": "2019-06-16",
+    "endDate": "2019-06-16", 									// could be null
+    "description": "Doing stuff",
+    "area": "Berlin", */}
+        </div>
+        <div>
+          <span className="h3 text-primary dRelative">
+            <GrFormEdit />
+          </span>
 
-                    <br/>
-                    <span><GoThreeBars/></span>
-                </div>
-            </div>
-    }
+          <br />
+          <span>
+            <GoThreeBars />
+          </span>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default DisplayList;
