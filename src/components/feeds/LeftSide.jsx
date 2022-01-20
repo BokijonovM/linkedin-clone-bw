6bea650c89f4417dae128ce22ceba1be66c8ab13
DonAzi <savaSh-00@mail.ru>
2022-01-20 10:02:41 +0000

    update

--- src/components/feeds/LeftSide.jsx
+++ src/components/feeds/LeftSide.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
+import BsBookmarkFill from "react-icons/bs";
 import "../profile/style/SideBar.css";
 
 function LeftSide({ profile }) {
@@ -16,15 +17,58 @@ function LeftSide({ profile }) {
               alt=""
             />
           </div>
-          <img
-            className="img-fluid user-image"
-            src={profile.image}
-            alt="user"
-          />
-          <div className="font-weight-bolder">
-            {profile.name} {profile.surname}
+          <a href="/#" className="text-dark">
+            <div className="user-img-wrapper">
+              <img
+                className="img-fluid user-image"
+                src={profile.image}
+                alt="user"
+              />
+            </div>
+            <div className="font-weight-bolder user-names">
+              {profile.name} {profile.surname}
+            </div>
+          </a>
+          <p className="user-title text-muted">{profile.title}</p>
+          <div className="second-part-user mt-1 mb-1">
+            <ul className="p-3">
+              <a className="text-dark a-link text-decoration-none" href="/#">
+                <li className="d-flex pl-0">
+                  <div className="text-muted font-weight-bolder">
+                    Who viewd your profile
+                  </div>
+                  <div className="ml-auto text-primary font-weight-bolder">
+                    58
+                  </div>
+                </li>
+              </a>
+              <a className="text-dark a-link text-decoration-none" href="/#">
+                <li className="d-flex">
+                  <div className="text-muted font-weight-bolder">
+                    Views of your post
+                  </div>
+                  <div className="ml-auto text-primary font-weight-bolder">
+                    177
+                  </div>
+                </li>
+              </a>
+            </ul>
           </div>
-          <p className="">{profile.title}</p>
+          <div className="pl-3 pt-1">
+            <a id="third-part-user" href="/#">
+              <h3>Access exclusice tools & insights</h3>
+              <span className="get-hired text-decoration-none">
+                Get Hired Faster, Try Premium Free
+              </span>
+            </a>
+          </div>
+          <div>
+            <a href="/#">
+              <span className="get-hired">
+                {/* <BsBookmarkFill />  */}My Items
+              </span>
+            </a>
+          </div>
         </div>
       </div>
     </>
