3e03e341fed8ed13173aa09b1fa76a11862460bb
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-01-21 01:51:06 +0000

    sidebar created

--- src/components/feeds/LeftSide.jsx
+++ src/components/feeds/LeftSide.jsx
@@ -1,17 +1,61 @@
 import React, { useState, useEffect } from "react";
-
+import {IoIosPeople} from 'react-icons/io'
 function LeftSide({profile}) {
 
   useEffect(()=> {
     console.log('Profile from left side' ,profile)
   })
-  return <div>
-    {profile.name}
-    {profile.surnamename}
-    {profile.bio}
-    
-   <div className="rounded" style={{width:'50px',height:'50px'}}> <img className="w-100" src={profile.image} alt='user '/></div>
-    </div>;
+  return (<>
+  <div className='bg-white round-border mt-2  w-100' style={{fontSize:'12px'}}>
+                <div className='d-flex'>
+              <img className='w-100' style={{borderRadius:'10px 10px 0 0'}}src="https://images.ctfassets.net/7thvzrs93dvf/wpImage18643/2f45c72db7876d2f40623a8b09a88b17/linkedin-default-background-cover-photo-1.png?w=790&h=196&q=90&fm=webp" alt='linkedin cover'/>
+                  </div>            
+            <div className="rounded m-auto pRelative"  style={{top:'-30px',width:'75px',height:'75px'}}> 
+            <img src={profile.image} alt='user' style={{borderRadius:'50%',border:'2px solid white',overflow:'hidden',width:'75px',height:'75px'}}/>
+            </div>
+                <p className='h5 bold px-3'>{profile.name} {profile.surnamename}</p>
+                <p>{profile.bio}</p>
+                <hr/>
+              <div  className="h6 px-3 d-flex justify-content-between">
+                <span> Who viewed your profile </span>
+                <span className='bold blue-link'>41</span></div>
+              <div className="h6 px-3 d-flex justify-content-between">
+                <span>Views of your post</span> 
+                <span className='bold blue-link'>328</span></div>
+                <hr/>
+                <p className="px-3 text-left" style={{fontSize:'16px'}}>Access exclusive tools & insights</p>
+                <div className='d-flex px-2'>
+                  {/* <div style={{minHeight:'10px',minWeight:'15px',color:'transparent',backgroundImage:'linear-gradient(210deg, rgb(231,163,62), rgb(247,199,125))'}}>aa</div> */}
+                  <div >
+                    <p className=' m-2' style={{maxHeight:'15px',minWeight:'15px',color:'transparent',backgroundImage:'linear-gradient(70deg, rgb(231,163,62), rgb(247,199,125))'}}>aa</p></div>
+                  <p className="h6 bold text-left">Get Hired Faster, Try Premium Free</p>
+                </div>
+                <hr/>
+                <p className='h5 text-left px-3 pb-3'> <i className="m-2 bi bi-bookmark-fill"></i>My items</p>
+        </div>
+        <div className='bg-white text-left round-border mt-3  w-100 p-3' style={{fontSize:'12px'}}>
+        <p>Recent</p>
+<p><IoIosPeople className='mr-2'/>React.JS Devs</p>
+<p><IoIosPeople className='mr-2'/>Frontend Developer and Web Developers</p>
+<p><IoIosPeople className='mr-2'/>Full Stack Developer - Java technologies, UI & Microservices</p>
+<p><IoIosPeople className='mr-2'/>Web Developer Jobs</p>
+<p><IoIosPeople className='mr-2'/>JavaScript Full Stack Developer</p>
+<p className='bold blue-link'>Groups</p>
+<p><IoIosPeople className='mr-2'/>React.JS Devs</p>
+<p><IoIosPeople className='mr-2'/>Frontend Developer and Web Developers</p>
+<p><IoIosPeople className='mr-2'/>Full Stack Developer</p> 
+<p><IoIosPeople className='mr-2'/>Java technologies</p>
+<p><IoIosPeople className='mr-2'/>UI & Microservices</p>
+<p>Show more</p>
+<p>Groups</p>
+<p>EventsSee all Events</p>
+
+<p>Followed Hashtags</p>
+<hr/>
+<div>Discover More</div>
+        </div>
+  
+    </>)
 }
 
 export default LeftSide;
--- src/components/feeds/MyMainFeed.jsx
+++ src/components/feeds/MyMainFeed.jsx
@@ -5,15 +5,16 @@ import NewsFeed from "./NewsFeed";
 import LeftSide from "./LeftSide";
 import RightSide from "./RightSide";
 import Footer from "./Footer";
+import Header from "../profile/Header/Header";
 
 function MyMainFeed({ profile }) {
   return (
     <div>
-      <MyNavbar />
+      <Header profile={profile}/>
       <Container>
         <Row className="justify-content-center">
-          <Col md={2}>{profile && <LeftSide profile={profile} />}</Col>
-          <Col className="px-4" md={7}>
+          <Col className='text-center' md={3}>{profile && <LeftSide profile={profile} />}</Col>
+          <Col className="px-4" md={6}>
             <NewsFeed />
           </Col>
           <Col md={3}>
