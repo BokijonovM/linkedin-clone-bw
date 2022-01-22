98d5886686f7697cbb2a4b41f7a2e8e9825b1235
bokijonovm <muhsinjonboqijonov2002@gmail.com>
2022-01-22 07:36:10 +0100

    updated

--- src/components/feeds/SingleNews.jsx
+++ src/components/feeds/SingleNews.jsx
@@ -1,24 +1,24 @@
 import React, { useEffect } from "react";
-import { Row, Button, Modal, Form } from "react-bootstrap";
+import { Row, Button, Modal, Form, Dropdown } from "react-bootstrap";
 import "./style.css";
 import { parseISO, format } from "date-fns";
-import PostDropDown from "./PostDropDown";
+// import PostDropDown from "./PostDropDown";
 import { useState } from "react";
 import { StylesContext } from "@material-ui/styles";
 
 function SingleNews({ posts, fetchData }) {
   const [addPost, setAddPost] = useState(false);
-  
+
   const showAddPost = () => setAddPost(true);
   const closeAddPost = () => setAddPost(false);
-  
-  const [postId, setPostId] = useState()
-  const [text,setText] = useState()
-  
+
+  const [postId, setPostId] = useState();
+  const [text, setText] = useState();
+
   useEffect(() => {
-   setPostId(posts._id)
-   setText(posts.text)
-    console.log("from single post", posts._id)
+    setPostId(posts._id);
+    setText(posts.text);
+    console.log("from single post", posts._id);
   }, []);
 
   const handleDeletePost = async () => {
@@ -36,7 +36,7 @@ function SingleNews({ posts, fetchData }) {
 
       if (response.ok) {
         setAddPost(false);
-       fetchData()
+        fetchData();
       } else if (response.status === 401)
         alert("You Can not Delete others Posts");
       else {
@@ -54,7 +54,7 @@ function SingleNews({ posts, fetchData }) {
         {
           method: "PUT",
           body: JSON.stringify({
-            text:text,
+            text: text,
           }),
           headers: {
             "Content-Type": "application/json",
@@ -66,7 +66,7 @@ function SingleNews({ posts, fetchData }) {
       if (response.status === 401) alert("you can not update others posts");
       if (response.ok) {
         closeAddPost();
-        fetchData()
+        fetchData();
       }
     } catch (error) {
       console.error(error);
@@ -74,9 +74,48 @@ function SingleNews({ posts, fetchData }) {
   };
 
   const handleEdit = id => {
-    
     showAddPost();
   };
+  const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
+    <a
+      style={{ color: "black" }}
+      href=""
+      ref={ref}
+      onClick={e => {
+        e.preventDefault();
+        onClick(e);
+      }}
+    >
+      {children}
+      {/* &#x25bc; */}
+    </a>
+  ));
+
+  // forwardRef again here!
+  // Dropdown needs access to the DOM of the Menu to measure it
+  const CustomMenu = React.forwardRef(
+    ({ children, style, className, "aria-labelledby": labeledBy }, ref) => {
+      const [value, setValue] = useState("");
+
+      return (
+        <div
+          ref={ref}
+          style={style}
+          className={className}
+          aria-labelledby={labeledBy}
+        >
+          <div className="mx-3 d-flex justify-content-center"></div>
+          <hr />
+          <ul className="list-unstyled">
+            {React.Children.toArray(children).filter(
+              child =>
+                !value || child.props.children.toLowerCase().startsWith(value)
+            )}
+          </ul>
+        </div>
+      );
+    }
+  );
 
   return (
     <div>
@@ -117,15 +156,128 @@ function SingleNews({ posts, fetchData }) {
                   </p>
                 </div>
                 <div className="ml-auto d-flex align-items-center">
-                  <Button
+                  {/* <Button
                     onClick={showAddPost}
                     className="py-0 shadow-none"
                     size="sm"
                     variant="secondary"
                   >
                     Edit
-                  </Button>
-                  <PostDropDown />
+                  </Button> */}
+                  <Dropdown alignRight>
+                    <Dropdown.Toggle
+                      alignRight
+                      as={CustomToggle}
+                      id="dropdown-custom-components"
+                      style={{ color: "black !important" }}
+                    >
+                      <i className="bi bi-three-dots"></i>
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu
+                      style={{ width: "240px" }}
+                      as={CustomMenu}
+                      alignRight
+                    >
+                      <div className="post-dropdown">
+                        <div
+                          onClick={showAddPost}
+                          className="d-flex align-items-center justify-content-between px-3 py-2 mx-2"
+                        >
+                          <i class="bi bi-pencil-square"></i>
+                          <div>
+                            <p
+                              className="mb-0"
+                              style={{ fontWeight: "600", fontSize: "12px" }}
+                            >
+                              Edit
+                            </p>
+                          </div>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between px-3 py-2 mx-2">
+                          <i class="bi bi-download"></i>
+                          <div>
+                            <p
+                              className="mb-n1"
+                              style={{ fontWeight: "600", fontSize: "12px" }}
+                            >
+                              Save
+                            </p>
+                            <p className="mb-0" style={{ fontSize: "10px" }}>
+                              Save for later
+                            </p>
+                          </div>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between px-3 py-2 mx-2">
+                          <i class="bi bi-link"></i>
+                          <div>
+                            <p
+                              className="mb-0"
+                              style={{ fontWeight: "600", fontSize: "12px" }}
+                            >
+                              Copy link to post
+                            </p>
+                          </div>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between px-3 py-2 mx-2">
+                          <i class="bi bi-code-slash"></i>
+                          <div>
+                            <p
+                              className="mb-n1"
+                              style={{ fontWeight: "600", fontSize: "12px" }}
+                            >
+                              Embed this post
+                            </p>
+                            <p className="mb-0" style={{ fontSize: "10px" }}>
+                              Copy and Paste embed to you site
+                            </p>
+                          </div>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between px-3 py-2 mx-2">
+                          <i class="bi bi-flag-fill"></i>
+                          <div>
+                            <p
+                              className="mb-n1"
+                              style={{ fontWeight: "600", fontSize: "12px" }}
+                            >
+                              Report this post
+                            </p>
+                            <p className="mb-0" style={{ fontSize: "10px" }}>
+                              I don't want to see this post
+                            </p>
+                          </div>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between px-3 py-2 mx-2">
+                          <i class="bi bi-ui-radios"></i>
+                          <div>
+                            <p
+                              className="mb-n1"
+                              style={{ fontWeight: "600", fontSize: "12px" }}
+                            >
+                              Improve my feed
+                            </p>
+                            <p className="mb-0" style={{ fontSize: "10px" }}>
+                              Get recommended source to follow
+                            </p>
+                          </div>
+                        </div>
+                        <div className="d-flex align-items-center justify-content-between px-3 py-2 mx-2">
+                          <i class="bi bi-eye-fill"></i>
+                          <div>
+                            <p
+                              className="mb-n1"
+                              style={{ fontWeight: "600", fontSize: "12px" }}
+                            >
+                              Who can see that post?
+                            </p>
+                            <p className="mb-0" style={{ fontSize: "10px" }}>
+                              Visible to anyone on or off LinkedIn
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </Dropdown.Menu>
+                  </Dropdown>
                 </div>
               </div>
               <div>
@@ -209,7 +361,7 @@ function SingleNews({ posts, fetchData }) {
                   value={text}
                   // onChange={e => setPost(e.target.value)}
                   rows={4}
-                  onChange={e => setText( e.target.value)}
+                  onChange={e => setText(e.target.value)}
                 />
               </Form.Group>
               <div className="d-flex justify-content-between px-3">
